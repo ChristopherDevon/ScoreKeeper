238e5c10e51921d9e12464c65da630591e69a1b6
ChristopherDevon <devonchen12@gmail.com>
2018-09-09 15:34:37 +0700

    initial commit

--- Scorekeeper.c
+++ Scorekeeper.c
@@ -1,5 +1,49 @@
 #include<stdio.h>
 #include<string.h>
 
-char username[5][100];
+main(){
+	char username[51]="rey1sCool";
+	char password[51]="prestige";
+	char u[51]="";
+	char p[51]="";
+	
+	do{
+	printf("\n\n\n\n\n\nUsername:");
+	scanf("%[^\n]");
+	getchar();
+	
+	printf("Password:");
+	scanf("%[^\n]");
+	getchar();
+	
+	}while (strcmp(u,username )!=0 || strcmp(p,password)!=0 );
+	
+	readFile();{
+	FILE *f = fopen("records.txt", "r");
+	
+	if(!f){
+		printf("No student record found!");
+		while(getchar() != '\n');
+		return;
+	} else {
+		while(!feof(f)){
+			fscanf(f, "%s %d %d\n", StudentScore);	
+			count++;
+		}
+	}
+	
+	fclose(f);
+}
+
+	view();{		
+	cls();
+	puts(" ScoreKeeper \n\n =======================================================");
+	puts("\n 1.View Student Score: \n 2.Update Student Score: \n 3.Save Student Score: \n 4.Exit \n Choice :");
+	
+	
+	}
+	
+	
+
+}
 
